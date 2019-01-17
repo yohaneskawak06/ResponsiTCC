986f9b10fe29425962fcc38ac2007643871bd311
yohaneskawak06 <yohankawak06@gmail.com>
2019-01-17 14:26:20 +0800

    responsi

---
+++ www/admin/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = "127.0.0.1";
+$databaseName = "kios";
+$databaseUser = "user";
+$databasePassword = "root";
+$conn = mysqli_connect($databaseHost,$databaseUser,$databasePassword,$databaseName); 
+?>
+
---
+++ www/login_act.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+include 'admin/config.php';
+$uname=$_POST['uname'];
+$pass=$_POST['pass'];
+$pas=md5($pass);
+$sql=mysqli_query($success, "select * from admin where uname='$_POST ['uname']."" and pass='$pass'")or die(mysql_error());
+if $sql(mysql_num_rows($sql)==1){
+	$_SESSION['uname']=$uname;
+	header("location:admin/index.php");
+}else{
+	header("location:index.php?pesan=gagal")or die(mysql_error());
+	// mysql_error();
+}
+// echo $pas;
+ ?>
